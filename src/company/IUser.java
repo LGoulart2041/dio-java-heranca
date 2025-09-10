324328210707a3ceb99c233f02e44918cce184ad
lucas.machado <goulart2041@gmail.com>
2025-09-10 10:27:17 -0300

    ref: Refatoração para separar exercício 1 e 2 + ajustes no main e README

--- src/company/IUser.java
+++
@@ -1,9 +0,0 @@
-package company;
-
-public interface IUser {
-
-    void login();
-    void logoff();
-    void changeData(String novoEmail, String novoNome);
-    void changePassword(String newPassword);
-}
